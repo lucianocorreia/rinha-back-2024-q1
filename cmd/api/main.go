7a45bdd6c6a1631ea560d1cc8b3ad026300cf385
Luciano Correia <luciano.correia@gmail.com>
2024-02-18 22:36:27 +0400

    add extrato endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -25,6 +24,27 @@ type (
 		Limite int `json:"limite"`
 		Saldo  int `json:"saldo"`
 	}
+
+	UltimasTransacoesResponse struct {
+		Valor       int       `json:"valor"`
+		Tipo        string    `json:"tipo"`
+		Descricao   string    `json:"descricao"`
+		RealizadaEm time.Time `json:"realizada_em"`
+	}
+
+	ExtratoResponse struct {
+		Saldo struct {
+			Total       int    `json:"total"`
+			DataExtrato string `json:"data_extrato"`
+			Limite      int    `json:"limite"`
+		} `json:"saldo"`
+		UltimasTransacoes []UltimasTransacoesResponse `json:"ultimas_transacoes"`
+	}
+)
+
+const (
+	SQL_LISTAR_CLIENTES   = "SELECT saldo, limite FROM clientes WHERE id = $1"
+	SQL_LISTAR_TRANSACOES = "SELECT valor, tipo, descricao, realizado_em AS realizado_em FROM transacoes  WHERE cliente_id = $1 ORDER BY realizado_em DESC LIMIT 10"
 )
 
 func main() {
@@ -35,6 +55,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	pc.MaxConns = 100
 
 	pool, err := pgxpool.NewWithConfig(context.Background(), pc)
 	if err != nil {
@@ -47,7 +68,7 @@ func main() {
 		Handler: routes(pool),
 	}
 
-	log.Println("Server running on port", port)
+	log.Println("Server running on port ", port)
 	log.Fatalln(srv.ListenAndServe())
 }
 
@@ -65,7 +86,7 @@ func handleTransacoes(pool *pgxpool.Pool) http.HandlerFunc {
 		p := r.PathValue("id")
 		cid, err := strconv.Atoi(p)
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
+			w.WriteHeader(http.StatusUnprocessableEntity)
 			return
 		}
 
@@ -76,12 +97,12 @@ func handleTransacoes(pool *pgxpool.Pool) http.HandlerFunc {
 
 		var trb Transacao
 		if err := json.NewDecoder(r.Body).Decode(&trb); err != nil {
-			w.WriteHeader(http.StatusBadRequest)
+			w.WriteHeader(http.StatusUnprocessableEntity)
 			return
 		}
 
 		if (trb.Tipo != "c" && trb.Tipo != "d") || trb.Valor <= 0 || len(trb.Descricao) == 0 || len(trb.Descricao) > 10 {
-			w.WriteHeader(http.StatusBadRequest)
+			w.WriteHeader(http.StatusUnprocessableEntity)
 			return
 		}
 		trb.ClienteID = cid
@@ -93,10 +114,9 @@ func handleTransacoes(pool *pgxpool.Pool) http.HandlerFunc {
 		defer cancel()
 
 		var limite, saldo *int
-		err = pool.QueryRow(ctx, "CALL criar_tr($1, $2, $3, $4)", trb.ClienteID, trb.Valor, trb.Tipo, trb.Descricao).Scan(&limite, &saldo)
+		err = pool.QueryRow(ctx, "CALL criar_tr($1, $2, $3, $4)", trb.ClienteID, trb.Valor, trb.Tipo, trb.Descricao).Scan(&saldo, &limite)
 		if err != nil {
-			fmt.Println(err)
-			w.WriteHeader(http.StatusInternalServerError)
+			w.WriteHeader(http.StatusUnprocessableEntity)
 			return
 		}
 
@@ -118,7 +138,60 @@ func handleTransacoes(pool *pgxpool.Pool) http.HandlerFunc {
 
 func handleExtrato(pool *pgxpool.Pool) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusNotImplemented)
+		p := r.PathValue("id")
+		cid, err := strconv.Atoi(p)
+		if err != nil {
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			return
+		}
+
+		if cid > 5 {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		var saldo, limite *int
+		err = pool.QueryRow(ctx, SQL_LISTAR_CLIENTES, cid).Scan(&saldo, &limite)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if saldo == nil || limite == nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		var ultimasTransacoes []UltimasTransacoesResponse
+		rows, err := pool.Query(ctx, SQL_LISTAR_TRANSACOES, cid)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var utr UltimasTransacoesResponse
+			err = rows.Scan(&utr.Valor, &utr.Tipo, &utr.Descricao, &utr.RealizadaEm)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			ultimasTransacoes = append(ultimasTransacoes, utr)
+		}
+
+		var extratoResponse ExtratoResponse
+		extratoResponse.Saldo.Total = *saldo
+		extratoResponse.Saldo.Limite = *limite
+		extratoResponse.Saldo.DataExtrato = time.Now().UTC().String()
+		extratoResponse.UltimasTransacoes = ultimasTransacoes
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(extratoResponse)
 	}
 }
 
